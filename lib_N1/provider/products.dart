e47a44c57b7b8319b9f75e882226628a2ac88be0
GuihgMartins <86018157+GuihgMartins@users.noreply.github.com>
2021-06-16 16:50:22 -0300

    Add files via upload

---
+++ lib_N1/provider/products.dart
@@ -0,0 +1,60 @@
+import 'package:magazine_luiza/models/product.dart';
+
+List<Product> listOfProducts = [
+  Product(
+      photo: "assets/images/celular.jpg",
+      name: "Smartphone Samsung Galaxy A71, 128GB, Azul, 6GB RAM",
+      description: "Tela 6,7*, Câm. Quádrupla + Câm. Selfie 32MP",
+      price: "1.979,97",
+      time: 10,
+      portion: "212,90",
+      ),
+  Product(
+    photo: "assets/images/geladeira.jpg",
+    name: "Geladeira Brastemp, Frost Free Duplex Evox",
+    description: "375L, BRM45, HKANA",
+    price: "2.698,00",
+    time: 10,
+    portion: "269,80",
+  ),
+  Product(
+    photo: "assets/images/fritadeira.jpg",
+    name: "Fritadeira Elétrica sem Óleo/Air Fryer Nell Smart",
+    description: "Preta, 2,4l com timer",
+    price: "229,90",
+    time: 5,
+    portion: "45,98",
+  ),
+  Product(
+      photo: "assets/images/play5.png",
+      name: "Playstation 5",
+      description: "Playstation 5 novo, últimas unidades, acompanha controle e todos os cabos",
+      price: "4.999,99",
+      time: 12,
+      portion: "416,58",
+      isFavorite: true),
+  Product(
+    photo: "assets/images/controle.png",
+    name: "Controle de Playstation 4",
+    description: "Controle de PS4 novo, branco versão retrô",
+    price: "499,99",
+    time: 10,
+    portion: "49,90",
+    isFavorite: true),
+  Product(
+      photo: "assets/images/ventilador.jpg",
+      name: "Ventilador de Parede e Mesa Mondial, Maxi Power",
+      description: "NV-15-6P-FB, 30cm, 3 velocidades",
+      price: "89,90",
+      time: 2,
+      portion: "44,95",
+      ),
+  Product(
+    photo: "assets/images/xbox.png",
+    name: "Xbox Series X",
+    description: "Xbox X novo, últimas unidades, acompanha controle, todos os cabos e Halo 4",
+    price: "4.999,99",
+    time: 12,
+    portion: "416,58",
+    isFavorite: true),
+];
